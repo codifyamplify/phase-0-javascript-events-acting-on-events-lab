3571072d9f8967064663e79ae8998cc5ea77225b
David Whitney <codifyamplify@gmail.com>
2022-08-02 18:02:43 -0400

    commit tree 0e477694035c506886d3fa4e60a78055fe13363b

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+// LLLLLLLLLLLLLLLLLLLLLL
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+// RRRRRRRRRRRRRRRRRRRRRRRRR
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+// LISTENER
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+})
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
